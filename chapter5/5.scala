9fa73bf9607deb4a13d845ba706c419abea462aa
Nelson Chen <crazysim@gmail.com>
2012-04-01 05:48:43 -0700

    more solutions

---
+++ chapter5/5.scala
@@ -0,0 +1,6 @@
+import scala.reflect.BeanProperty
+
+class Student {
+  @BeanProperty var name = "Nobody"
+  @BeanProperty var id = 0
+}
---
+++ chapter5/6.scala
@@ -0,0 +1,7 @@
+// Not sure what class it is referring to in section 5.1. This isn't but a guess at what it might
+// have meant.
+class Person(var age: Int) {
+  if (age < 0) {
+    val age = 0
+  }
+}
